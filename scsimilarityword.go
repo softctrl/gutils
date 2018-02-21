72a96d79740458e5d7fd5e32b3bfc17f3056497d
Timoshenko <softctrl@users.noreply.github.com>
2018-02-21 11:13:14 -0300

    Update scsimilarityword.go

--- scsimilarityword.go
+++ scsimilarityword.go
@@ -15,7 +15,7 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // Lesser General Public License for more details.
 //
-package gutils
+package scgotils
 
 //
 // Calculate the similarity of two words, under the SC Algorithm :D
